eec909cc475d87c9b5d13edce761448b8ef29045
Nomu1Nomu1 <isaiah.tino@gmail.com>
2025-07-21 01:27:17 +0700

    message AI

--- routes/web.js
+++ routes/web.js
@@ -1,11 +1,10 @@
 import e from "express";
-import { chatMessage, newPersona } from "../controller/messageController.js";
+import { messageAi } from "../controller/messageController.js";
 import { createPersona, createUserPersona } from "../controller/modelController.js";
 
 const router = e.Router();
 
-router.post("/chat/message", chatMessage);
-router.post("/chat/start", newPersona);
+router.post("/chat/message", messageAi);
 router.post("/create/model-persona", createPersona)
 router.post("/create/user-persona", createUserPersona);
 
