858825854db783462c1ee3114e3ebeba637419e4
Nomu1Nomu1 <isaiah.tino@gmail.com>
2025-08-03 19:07:58 +0700

    yapping

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import e from 'express';
-import dotenv from 'dotenv';
-import router from './routes/web.js';
+import e from "express";
+import dotenv from "dotenv";
+import router from "./routes/web.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,7 +9,13 @@ const app = e();
 const port = process.env.PORT;
 
 app.use(e.json());
-
+app.use(
+  cors({
+    origin: `http://localhost:3000`,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(router);
 
 app.get("/", (req, res) => {
@@ -16,5 +23,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
