bd1e384dde875c60d3c66bc31a01637f3f547f04
Nomu1Nomu1 <isaiah.tino@gmail.com>
2026-02-04 17:17:17 +0700

    update voice controller

--- controller/voiceController.js
+++ controller/voiceController.js
@@ -1,5 +1,22 @@
 import axios from "axios";
+import { error } from "console";
 import FormData from "form-data";
 import fs from "fs"
 
 const TTS_SERVER_URL = process.env.TTS_SERVER_URL
+
+export const uploadVoiceSample = async(req, res)  => {
+    try {
+        if(!req.file) {
+            return res.status(400).json({
+                error: "No audio file uploaded"
+            })
+        }
+
+        const voiceName = req.body.voice_name || "rina"
+
+        const formData = new FormData
+    } catch (error) {
+        
+    }
+}
